989507d546fa6cb0156c42241368aede29f1ba2d
machindra7 <150156282+machindra7@users.noreply.github.com>
2026-03-29 15:07:57 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -43,6 +43,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<body>");
 		out.println("<h1> welcome to Devops Training</h1>");
 		out.println("<h1> I love India</h1>");
+		out.println("<h1> My name is Machindra</h1>");
 
 				out.println("</body>");
 		out.println("</html>");
